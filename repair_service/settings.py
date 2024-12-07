9bbf51c356eaa36d1e0f0fede77c174d273c24ce
programmistvdzinsahirybashke <ilnaz09.ys@gmail.com>
2024-12-08 00:39:19 +0300

    1

--- repair_service/settings.py
+++ repair_service/settings.py
@@ -92,10 +92,10 @@ WSGI_APPLICATION = 'repair_service.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres3',  # замените на имя вашей базы данных
+        'NAME': 'postgres',  # замените на имя вашей базы данных
         'USER': 'postgres',  # ваш логин
         'PASSWORD': '123',  # ваш пароль
-        'HOST': '94.141.160.109',
+        'HOST': 'localhost',
         'PORT': '5432',  # по умолчанию
     }
 }
